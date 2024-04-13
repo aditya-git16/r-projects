697cb2a5570123191fcec17bff52241a292c2e92
adi16an <es1221211@iitd.ac.in>
2024-04-13 23:31:33 +0530

    sniffer complete

--- ip_sinffier/src/main.rs
+++ ip_sinffier/src/main.rs
@@ -1,4 +1,10 @@
-use std::{env, net::IpAddr , str::FromStr , process};
+use std::{env, io, net::{IpAddr , TcpStream}, process, str::FromStr};
+use std::thread;
+use std::sync::mpsc::{Sender , channel};
+use std::io::{Read,Write};
+
+// Max port we can sniff
+const MAX: u16 = 65535;
 
 struct Arguements{
     flag : String,
@@ -27,9 +33,9 @@ impl Arguements{
         }else {
             let flag = args[1].clone();
             if flag.contains("-h") || flag.contains("-help") && args.len() == 2 {
-                println!("Usage : -j to select how many threads tou want
+                println!("Usage : -j to select how many threads you want
                 \r\n      -h or -help to show this help message");
-                return Err("Help");
+                return Err("help");
             }else if flag.contains("-h") || flag.contains("-help"){
                 return Err("Too many arguements");
             }else if flag.contains("-j") {
@@ -49,17 +55,71 @@ impl Arguements{
         }
 }  
 
+// The scan function
+fn scan(tx: Sender<u16> , start_port: u16 , addr: IpAddr , num_threads: u16){
+    let mut port = start_port + 1;
+    loop{
+        match TcpStream::connect((addr , port)){
+            Ok(_) => {
+                print!(".");
+                io::stdout().flush().unwrap();
+                tx.send(port).unwrap();
+            }
+            Err(_) => {}
+
+        }
+
+        if (MAX - port)  <= num_threads {
+            break;
+        }
+        port += num_threads;
+
+    }
+}
 fn main() {
     let args : Vec<String> = env::args().collect();
+
     let program = args[0].clone();
+
     let arguements = Arguements::new(&args).unwrap_or_else(
         |err| {
             if err.contains("help"){
                 process::exit(0);
             }else {
-                eprintln!("Problem parsing arguements: {}", err);
+                eprintln!("{} Problem parsing arguements: {}", program ,  err);
                 process::exit(0);
             }
         }
     );
+
+    // binding the number of threads to a variable
+    let num_threads = arguements.threads;
+
+    //ip address allocation
+    let addr = arguements.ipaddr;
+
+    //now will make a channel
+    // tx is the transmitter and rx is the receiver
+
+    let (tx , rx) = channel();
+    for i in 0..num_threads{
+        // we will clone the transmitter so that every thread has its own transmitter
+        let tx = tx.clone();
+
+        thread::spawn(move || {
+            scan(tx , i, addr , num_threads);
+        });
+    }
+
+    let mut out = vec![];
+    drop(tx);
+    for p in rx{
+        out.push(p);
+    }
+
+    println!("");
+    out.sort();
+    for v in out{
+        println!("{} is open" , v);
+    }
 }
